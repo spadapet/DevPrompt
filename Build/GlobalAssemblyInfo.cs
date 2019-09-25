c4e54b37344d018c9a498289415c58851d06bb96
Peter Spada <peterspa@microsoft.com>
2019-09-25 12:25:21 -0700

    v1.5.4

--- Build/GlobalAssemblyInfo.cs
+++ Build/GlobalAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyProduct("DevPrompt")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("1.5.3.0")]
+[assembly: AssemblyVersion("1.5.4.0")]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en", UltimateResourceFallbackLocation.MainAssembly)]
